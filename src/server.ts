8876dab3408016574904faed5abe3c4a87bdfdaf
luise-go <luise.goede@yahoo.de>
2021-05-11 14:48:07 +0200

    :heavy_plus_sign: npm i inquirer

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,10 @@
-import fs from 'fs';
-
-console.log(fs.readFileSync('./README-md', 'utf-8'));
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+readline.question('What is your password?', (password: string) => {
+  readline.question('What do you want to do', (command: string) => {
+    console.log(`You like to ${command}this password:${password}`);
+    readline.close();
+  });
+});
